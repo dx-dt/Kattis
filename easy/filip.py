78f5adb29c3815035b5703906d296fc8edcbcf67
Peter Eklund <peter.eklund@ideoflux.com>
2018-10-31 06:29:44 +0100

    Added solution in Python for problem 'filip'.

---
+++ easy/filip.py
@@ -0,0 +1,10 @@
+# https://open.kattis.com/problems/filip
+
+from sys import stdin
+
+a,b = stdin.readline().rstrip().split(' ')
+
+if int(a[::-1]) > int(b[::-1]):
+    print a[::-1]
+else:
+    print b[::-1]
